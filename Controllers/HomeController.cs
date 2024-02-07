6cbc09d557da522f553c1d3bbd4fba521a35f834
Abhishek Hanumant Patil <abhishek.hpatil63@gmail.com>
2024-02-07 20:31:45 +0530

    Create all APIs for performing CRUD operations. Also add basic view for list by calling Index method.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 
 namespace CRUD_Basic.Controllers
 {
     public class HomeController : Controller
     {
+        MVCDBContext _MVCDBContext = new MVCDBContext();
         public ActionResult Index()
         {
+            var listOfStudents = _MVCDBContext.StudentDatas.ToList();
+            return View(listOfStudents);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(StudentData studentData)
         {
-            ViewBag.Message = "Your application description page.";
-
+            _MVCDBContext.StudentDatas.Add(studentData);
+            _MVCDBContext.SaveChanges();
+            ViewBag.Message = "Data successfully inserted.";
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult Edit(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            var tableData = _MVCDBContext.StudentDatas.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(tableData);
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult Edit(StudentData studentData)
+        {
+            var tableData = _MVCDBContext.StudentDatas.Where(x => x.StudentId == studentData.StudentId).FirstOrDefault();
+            if (tableData != null)
+            {
+                tableData.StudentName = studentData.StudentName;
+                tableData.StudentFees = studentData.StudentFees;
+                tableData.StudentHomeCity = studentData.StudentHomeCity;
+                _MVCDBContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var tableData = _MVCDBContext.StudentDatas.Where(x => x.StudentId == id).FirstOrDefault();
+            return View(tableData);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var tableData = _MVCDBContext.StudentDatas.Where(x => x.StudentId == id).FirstOrDefault(x => x.StudentId == id);
+            _MVCDBContext.StudentDatas.Remove(tableData);
+            _MVCDBContext.SaveChanges();
+            ViewBag.Message = "Record Delete Successfully.";
+            return RedirectToAction("Index");
         }
     }
 }
